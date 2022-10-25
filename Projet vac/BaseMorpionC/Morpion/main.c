ae7ef2fc137b873ff11ed1794f52c544943fcea9
Djquinquin <quentinpol140@gmail.com>
2022-10-25 13:10:11 +0200

    1 v 1 player V1(finish)

--- Projet vac/BaseMorpionC/Morpion/main.c
+++ Projet vac/BaseMorpionC/Morpion/main.c
@@ -17,38 +17,77 @@ int main(int argc, char ** argv)
 		printf("Choissisez une des options suivantes :\n");
 		printf("1 - Jouer contre un ami ?\n2 - Jouer contre une IA ?\n3 - En savoir plus sur le morpion ?\n\n");
 
-		nbStart = getch(); // Wait user tap number
+		scanf("%d", &nbStart); // Wait user tap number
 
-		int infini = 1, index;
+		int index, win = 0, null = 0;
 
-		if (nbStart == 49) // Equivalent number 1
+		if (nbStart == 1) // Equivalent number 1
 		{
 			printf("Vous avez choisi 1 !\n\n");
 
 			initializeGameState(grille); // Initialize grid
 
+			char player1 = 'X', player2 = 'O'; // Symbol player 1 and 2
+			printf("Le Joueur 1 aura le symbole X et le joueur 2 le symbole O\n\n");
+
 			do
 			{
-				char player1 = 'X', player2 = 'O'; // Symbol player 1 and 2
-				printf("Le Joueur 1 aura le symbole X et le joueur 2 le symbole O\n\n");
+				int nbPlayer = 1; // Player 1
 
-				int nbPlayer = 1;
+				// Show grid before turn Player 1
+				for (int i = 0; i < 9; i++)
+				{
+					if (grille[i] == 0)
+					{
+						printf("- ");
+					}
+					else if (grille[i] == 1)
+					{
+						printf("X ");
+					}
+					else if (grille[i] == -1)
+					{
+						printf("O ");
+					}
+
+					if (i == 2 || i == 5 || i == 8)
+					{
+						printf("\n");
+					}
+				}
 
 				printf("\nTour actuel : Joueur 1\n\nRAPPEL : X = la colonne (donc 0 a 2 (3 colonnes)) et Y = la ligne (donc 0 a 2 (3 lignes)), exemple 11 = milieu\nRentrer la position de votre symbole (x y) : ");
 				scanf("%d %d", &positionX, &positionY);
-				while (positionX > 2 || positionX < 0 || positionY > 2 || positionY < 0)
-				{
-					printf("Veuillez rentrer une valeur de X ou Y inferieur a 3 ou superieur ou egal a 0");
-					scanf("%d%d", &positionX, positionY);
-				}
 
 				index = (positionY * 3) + positionX; // Formule tab 2 dimensions to 1 dimension
+
+				while (positionX > 2 || positionX < 0 || positionY > 2 || positionY < 0 || grille[index] != 0)
+				{
+					printf("Veuillez rentrer une valeur de X ou Y inferieur a 3 ou bien superieur ou egal a 0 et une case non jouee : ");
+					scanf("%d %d", &positionX, &positionY);
+
+					index = (positionY * 3) + positionX; // Formule tab 2 dimensions to 1 dimension
+				}
+
 				grille[index] = nbPlayer; // Stock
 
-				nbPlayer *= -1;
+				if (testGameEnd(grille) == 1)
+				{
+					printf("\nJoueur 1 Gagnant ! GG\n\n");
+					win = 1;
+					break;
+				}
+				else if (testGameEnd(grille) == -2)
+				{
+					printf("\nMatch nul ! Bien jouez a vous !\n\n");
+					null = 1;
+					break;
+				}
 
-				printf("\n\nVoici la grille apres le joueur 1\n\n");
-				// Show grid after turn Player 1
+
+				nbPlayer *= -1; // Player 2
+
+				// Show grid before turn Player 2
 				for (int i = 0; i < 9; i++)
 				{
 					if (grille[i] == 0)
@@ -72,32 +111,72 @@ int main(int argc, char ** argv)
 
 				printf("\nTour actuel : Joueur 2\n\nRAPPEL : X = la colonne (donc 0 a 2 (3 colonnes)) et Y = la ligne (donc 0 a 2 (3 lignes)), exemple 11 = milieu\nRentrer la position de votre symbole (xy) : ");
 				scanf("%d %d", &positionX, &positionY);
-				while (positionX > 2 || positionX < 0 || positionY > 2 || positionY < 0)
-				{
-					printf("Veuillez rentrer une valeur de X ou Y inferieur a 3 ou superieur ou egal a 0");
-					scanf("%d%d", &positionX, positionY);
-				}
 
 				index = (positionY * 3) + positionX; // Formule tab 2 dimensions to 1 dimension
+
+				while (positionX > 2 || positionX < 0 || positionY > 2 || positionY < 0 || grille[index] != 0)
+				{
+					printf("Veuillez rentrer une valeur de X ou Y inferieur a 3 ou bien superieur ou egal a 0 et une case non jouee : ");
+					scanf("%d %d", &positionX, &positionY);
+
+					index = (positionY * 3) + positionX; // Formule tab 2 dimensions to 1 dimension
+				}
+
 				grille[index] = nbPlayer; // Stock
 
-			} while (infini == 1); // testGameEnd(grille) != -1 || testGameEnd(grille) != -2
+				if (testGameEnd(grille) == -1)
+				{
+					printf("\nJoueur 2 Gagnant ! GG\n\n");
+					win = 1;
+					break;
+				}
+				else if (testGameEnd(grille) == -2)
+				{
+					printf("\nMatch nul ! Bien jouez a vous !\n\n");
+					null = 1;
+					break;
+				}
+
+			} while (win != 1 || null != 1); // !Win or !null
+
+
+			// Show grid after win player or match null
+			for (int i = 0; i < 9; i++)
+			{
+				if (grille[i] == 0)
+				{
+					printf("- ");
+				}
+				else if (grille[i] == 1)
+				{
+					printf("X ");
+				}
+				else if (grille[i] == -1)
+				{
+					printf("O ");
+				}
+
+				if (i == 2 || i == 5 || i == 8)
+				{
+					printf("\n");
+				}
+			}
 
 		}
-		else if (nbStart == 50) // Equivalent number 2
+		else if (nbStart == 2) // Equivalent number 2
 		{
 			printf("Vous avez choisi 2 !\n\n");
 			printf("Bonne chance contre l'IA !\n\n");
 
 			initializeGameState(grille);
 		}
-		else if (nbStart == 51) // Equivalent number 3
+		else if (nbStart == 3) // Equivalent number 3
 		{
 			printf("Vous avez choisi 3 !\n\n");
 
 			printf("But du jeu : Le but du jeu est d aligner avant son adversaire 3 symboles identiques horizontalement, verticalement ou en diagonale. Chaque joueur a donc son propre symbole, generalement une croix pour l un et un rond pour l autre.\n\n");
-			printf("Le truc en + : Attention, le joueur qui débute est toujours avantage pour gagner. Pensez donc a alterner pour equilibre le combat !\n\n");
-			printf("En fin de partie : Le premier joueur a aligner 3 symboles identiques gagne la partie et marque 1 point. Si la grille est completee sans vainqueur, la partie est finie et il y a egalite.\n\n");
+			printf("Le truc en + : Attention, le joueur qui debute est toujours avantager pour gagner !\n\n");
+			printf("En fin de partie : Le premier joueur a aligner 3 symboles identiques gagne la partie. Si la grille est completee sans vainqueur, la partie est finie et il y a match nul.\n\n");
 		}
 
 
--- Projet vac/BaseMorpionC/Morpion/tictactoehelper.c
+++ Projet vac/BaseMorpionC/Morpion/tictactoehelper.c
@@ -9,7 +9,6 @@ void initializeGameState(int * gameState)
 	{
 		gameState[index] = 0;
 	}
-
 }
 
 // If a player win or match null
@@ -17,9 +16,56 @@ int testGameEnd(int * gameState)
 {
 	int nbEvaluate = 0;
 
+	// For player 1 (Inline, Column, Diagonal)
+	if ((gameState[0] == 1 && gameState[1] == 1 && gameState[2] == 1 )|| (gameState[3] == 1 && gameState[4] == 1 && gameState[5] == 1) || (gameState[6] == 1 && gameState[7] == 1 && gameState[8] == 1))
+	{ 
+		nbEvaluate = 1;
+	}
+	else if ((gameState[0] == 1 && gameState[3] == 1 && gameState[6] == 1) || (gameState[1] == 1 && gameState[4] == 1 && gameState[7] == 1) || (gameState[2] == 1 && gameState[5] == 1 && gameState[8] == 1))
+	{
+		nbEvaluate = 1;
+	}
+	else if ((gameState[0] == 1 && gameState[4] == 1 && gameState[8] == 1) || (gameState[2] == 1 && gameState[4] == 1 && gameState[6] == 1))
+	{
+		nbEvaluate = 1;
+	}
+
+
+
+	// For player 2 (Inline, Column, Diagonal)
+	if ((gameState[0] == -1 && gameState[1] == -1 && gameState[2] == -1) || (gameState[3] == -1 && gameState[4] == -1 && gameState[5] == -1) || (gameState[6] == -1 && gameState[7] == -1 && gameState[8] == -1))
+	{
+		nbEvaluate = -1;
+	}
+	else if ((gameState[0] == -1 && gameState[3] == -1 && gameState[6] == -1) || (gameState[1] == -1 && gameState[4] == -1 && gameState[7] == -1) || (gameState[2] == -1 && gameState[5] == -1 && gameState[8] == -1))
+	{
+		nbEvaluate = -1;
+	}
+	else if ((gameState[0] == -1 && gameState[4] == -1 && gameState[8] == -1) || (gameState[2] == -1 && gameState[4] == -1 && gameState[6] == -1))
+	{
+		nbEvaluate = -1;
+	}
+
+
+
+	// For match null
+	int count = 0;
+
+	for (int i = 0; i < 9; i++)
+	{
+		if (gameState[i] != 0)
+		{
+			count++;
+		}
+	}
+
+	if (count == 9)
+	{
+		nbEvaluate = -2;
+	}
+
 	return nbEvaluate;
 }
-
 
 int * copyGameState(int * gameState)
 {
@@ -29,7 +75,6 @@ int * copyGameState(int * gameState)
 	 newGameState = gameState;
 
 	// We return copy of grid
-
 	return newGameState;
 }
 
