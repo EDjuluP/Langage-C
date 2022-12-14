bb72909603b472127fd4121c2cb952cd347b568d
Djquinquin <quentinpol140@gmail.com>
2022-12-14 12:29:37 +0100

    JeuDes

---
+++ Projet vac/JeuDes/functions.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#include "functions.h"
+
+void saisieNomsJoueurs(Players ** Joueur, int nbJoueur)
+{
+
+}
---
+++ Projet vac/JeuDes/functions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct
+{
+	char name[50];
+
+
+} Players;
+
+void saisieNomsJoueurs(Players ** Joueur, int nbJoueur);
